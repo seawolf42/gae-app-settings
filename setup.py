bed1ae218fd420c42be63c075527718227e3d832
jeffrey k eliasen <jeff@jke.net>
2018-02-04 14:00:01 -0800

    Feature/continuous integration (#1)
    * travis file for integration tests
    
    * allow continuous integration to work even if README.rst is not present
    
    * do not use bare except
    
    * reference correct file in main test pass
    
    * adjust python path
    
    * correct test for file

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ import os
 from setuptools import find_packages, setup
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
+try:
+    with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+        README = readme.read()
+except Exception:
+    README = '<failed to open README.rst>'
+
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
